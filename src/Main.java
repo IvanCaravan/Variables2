da97dde18d415aac00a7030ac0ab3c3ba09d7b08
IvanCaravan <ivan.kirsanov.2000@mail.ru>
2023-02-08 19:16:27 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int apples = 1;
+        byte bananas = 2;
+        short chips = 300;
+        long dragons = 4000;
+        float e = 5.5f;
+        double f = 6.66;
+        System.out.println("Значение переменной apples с типом int равно " + apples);
+        System.out.println("Значение переменной bananas с типом byte равно " + bananas);
+        System.out.println("Значение переменной chips с типом short равно " + chips);
+        System.out.println("Значение переменной dragons с типом long равно " + dragons);
+        System.out.println("Значение переменной е с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
+    }
+
+}
